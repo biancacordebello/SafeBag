930c0f0d20e05dce1602ed786d9571b20764e6d5
Bianca Cordebello <biancagcordebello@gmail.com>
2024-08-09 01:10:34 -0300

    add search  in main and changed html files to php

---
+++ assets/partials/navbar.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/web/navbar.css">
+    <link rel="stylesheet" href="../styles/mobile/navbar.css">
+    <title>SafeBag</title>
+</head>
+<body>
+  <script src="https://kit.fontawesome.com/267c55b2ea.js" crossorigin="anonymous"></script>
+
+    <div class="navbar">
+        <div class="navbar_link-brand">
+          SafeBag
+        </div>
+        <div class="navbar_link_toggle" id="burguer-menu">
+          <i class="fas fa-bars"></i>
+        </div>    
+        <nav class="navbar_items navbar_toggleShow">
+        <div class="navbar_link">
+          Todos Alertas
+        </div>
+        <div class="navbar_link">
+          Alertas Recentes
+        </div>
+        <div class="navbar_link">
+          Cadastro de Bagagem
+        </div>
+    </nav>
+  </div>
+  
+  <script src="../js/navbar.js"></script>
+
+</body>
+</html>
---
+++ pages/main.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/styles/web/main.css">
+    <link rel="stylesheet" href="../assets/styles/web/navbar.css">
+
+
+    <title>SafeBag</title>
+
+    <?php include '../assets/partials/navbar.php'; ?> 
+
+</head>
+<body>
+
+    <div class="container">
+    <h1 id="main-title">Todos os Alertas</h1>
+
+    <div class="container-right">
+    <p id="search-title">CPF do proprietário ou código SAFE</p>
+    <div id="divBusca">
+        <input type="text" id="txtBusca" placeholder="Buscar..."/>
+        <button id="btnBusca">Buscar</button>
+      </div>
+    </div>
+</div>
+</body>
+</html>
