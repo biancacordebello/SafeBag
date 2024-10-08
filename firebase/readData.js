6241cc7d2cdbad1482539cb8499f32035df9e54c
Felipe <felipevazferreira68@gmail.com>
2024-10-08 18:19:39 -0300

    Implementação da ida a pagina de status da bagagem

--- firebase/readData.js
+++ firebase/readData.js
@@ -14,7 +14,7 @@ function lerDadosMala(){
                 const notificaElement = document.createElement('button');
                 
                 notificaElement.classList.add('id-mala');
-                notificaElement.textContent = notificaId;
+                notificaElement.textContent = 'Bagagem SAFE' + notificaId;
                 
 
                 const notificaContent = document.createElement('div');
@@ -23,16 +23,27 @@ function lerDadosMala(){
                 notificaP.textContent= notificaTexto;
                 notificaContent.appendChild(notificaP);
 
-                notificaElement.addEventListener('click', () => {
-                    notificaElement.classList.toggle('active');
+                const icon = document.createElement('span');
+                icon.classList.add('icon');
+                notificaElement.appendChild(icon);
+
+                icon.addEventListener('click', ()=>{
+                  event.stopPropagation();
+                  icon.classList.toggle('active');
                   
                     if (notificaContent.style.maxHeight) {
                       notificaContent.style.maxHeight = null; 
                     } else {
                       notificaContent.style.maxHeight = notificaContent.scrollHeight + "px"; 
                     }
+                });
+
+               
+                notificaElement.addEventListener('click', () => {
+                  window.location.href = 'bag_details.html';
                   });
 
+                  
 
             
                 idMala.appendChild(notificaElement);
